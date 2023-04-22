7804f46bef0432f30c71a60e86c8231f8878b32f
noverko <noverko@protonmail.com>
2023-04-22 14:12:59 +0300

    Init extension

--- popup/popup.js
+++ popup/popup.js
@@ -1,24 +1,23 @@
-// Initialize button with users' preferred color
 const changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', ({ color }) => {
+chrome.storage.sync.get('color', ({color}) => {
     changeColor.style.backgroundColor = color;
 });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener('click', async () => {
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+
+    console.log(tab);
 
     chrome.scripting.executeScript({
-        target: { tabId: tab.id },
+        target: {tabId: tab.id},
         func: setPageBackgroundColor
     });
 });
 
-// The body of this function will be executed as a content script inside the
-// current page
 function setPageBackgroundColor() {
-    chrome.storage.sync.get('color', ({ color }) => {
+    console.log(chrome.storage);
+    chrome.storage.sync.get('color', ({color}) => {
         document.body.style.backgroundColor = color;
     });
 }
